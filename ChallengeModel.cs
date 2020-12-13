2fb663abc0e1f40cba5ded1fe6ae919bd1da26cd
Ghazi Majdoub <majdoub.ghazi@gmail.com>
2020-12-13 14:43:36 -0500

    initial commit

---
+++ ChallengeModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DicteeSChallengeCreator
+{
+    class ChallengeModel
+    {
+        public List<ExerciceModel> Exs { get; set; }
+        public string Language { get; set; } // AR, EN, FR
+
+        // Challenge logic
+        public bool PassAlways { get; set; } // TRUE: Pass Always / FALSE: Pass only for correct answer
+        public bool EnableScore { get; set; }
+        public bool EnableTimer { get; set; }
+        public bool ScoreWithTiming { get; set; } // When TRUE: the time left in timer for each exercice is added to the score
+        public bool EnableCorrection { get; set; } // Show correction for each exercice when unanswered
+        public int AttemptsLimit { get; set; } // Max number of attempts allowed per exercice
+        public bool ShowKeyboard { get; set; } // TRUE: Show the answer letters buttons mixed with noise
+    }
+}
---
+++ ExerciceModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DicteeSChallengeCreator
+{
+    class ExerciceModel
+    {
+        public string Answer { get; set; }
+        public string Question { get; set; } // path to audio file
+        public string Noise { get; set; } // Noise pattern
+        public string Hint { get; set; } // path to hint image file
+        public string AnswerIntro { get; set; } // Introductory text showing before answer: "This is a "[...ANSWER_HERE...]
+
+        public int Score { get; set; } // score points given for correct answer
+        public int Time { get; set; } // Max time in seconds to enter correct answer
+        public int AttemptsLimit { get; set; } // Max number of attempts allowed
+        public bool ShowKeyboard { get; set; } // TRUE: Show the answer letters buttons mixed with noise
+    }
+}
